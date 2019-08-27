64f8830c42da0dcdc40e4a8ad673a45f85a4d131
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-08-26 20:15:02 -0500

    primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoGenerico';
+  nombre: string = '';
+  paises: any = [
+    { activo: true, nombre: 'Colombia' },
+    { activo: true, nombre: 'Brasil' },
+    { activo: true, nombre: 'Argentina' },
+    { activo: false, nombre: 'Polonia' },
+    { activo: false, nombre: 'Alemania' },
+    { activo: true, nombre: 'Francia' }
+  ]
+  lat:number = 51.678418;
+  lng:number = 7.809007; 
 }
